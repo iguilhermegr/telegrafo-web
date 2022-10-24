fd12092a485bb0ef3c013ffd2232c6c27fd662e0
guihrib <gr.miraidevelopment@gmail.com>
2022-10-24 09:10:12 -0300

    new changes

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,15 +1,52 @@
+const morses =     {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----",
+    " ": "",
+}
+
 function convert () {
     let textarea = document.getElementById('code-morse')
 
     function cMorse() {
-        document.getElementById("code-morse").innerHTML = "-.-."
+        let text = document.getElementById("code-morse").value.toLowerCase()
+        document.getElementById("code-morse-result").innerHTML = text.split("").map(a => a.replace(a, morses[a])).join(" ")
     }
     textarea.addEventListener('input', (e) => {
-        if (e.data === 'c'){
-            cMorse()
-        } else {
-            console.log('Não foi C')
-        }
+            if(morses[e.data]) cMorse()
     })
 }
 
